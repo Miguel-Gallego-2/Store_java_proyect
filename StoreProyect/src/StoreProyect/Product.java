be653de5b62797b4fd27f195b53be44bbbec70da
Miguel-Gallego-2 <miguel.gallego2@udea.edu.co>
2024-04-03 09:42:10 -0500

    Basic Structure

---
+++ StoreProyect/src/StoreProyect/Product.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StoreProyect;
+
+/**
+ *
+ * @author migue
+ */
+public class Product {
+    String name;
+    String Price;
+    int cuantity;
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String Price) {
+        this.Price = Price;
+    }
+
+    public int getCuantity() {
+        return cuantity;
+    }
+
+    public void setCuantity(int cuantity) {
+        this.cuantity = cuantity;
+    }
+    
+    
+}
